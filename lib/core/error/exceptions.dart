2643c843e645960797a13cc9fa02c8833ac8c38f
vickskamble <vikaskamble1324@gmail.com>
2026-08-03 11:49:29 -0700

    surface real import error: AppException toString + pushLogs detail

--- lib/core/error/exceptions.dart
+++ lib/core/error/exceptions.dart
@@ -2,6 +2,10 @@ sealed class AppException implements Exception {
   final String message;
   final String? code;
   const AppException(this.message, {this.code});
+
+  @override
+  String toString() =>
+      code == null ? message : '$message ($code)';
 }
 
 final class LocalStorageException extends AppException {
