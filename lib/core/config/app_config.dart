7594c62f87a9b19d613ce770f3ed31f01fb8ac17
vickskamble <vikaskamble1324@gmail.com>
2026-08-19 12:53:22 +0530

    chore: bump version to 1.2.3+23 (fix release)

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -10,7 +10,7 @@ class AppConfig {
   AppConfig._();
 
   /// Current app version, shown in Settings > System. Keep in sync with pubspec.
-  static const String appVersion = '1.1.17';
+  static const String appVersion = '1.2.3';
 
   static double _tariffPerUnit = AppConstants.tariffPerUnit;
   static double _demandChargePerKva = AppConstants.demandChargePerKva;
